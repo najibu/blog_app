e346841769c17f0e243b051f724c50dbc3b6baa1
najibu <ns.najibu@gmail.com>
2017-01-16 18:35:53 +0000

    edit bug fix

--- resources/views/backend/users/edit.blade.php
+++ resources/views/backend/users/edit.blade.php
@@ -4,51 +4,62 @@
 @section('content')
   <div class="container col-md-6 col-md-offset-3">
     <div class="well well bs-component">
-      <form method="post" class="form-horizontal">
-        {!! csrf_field() !!}
-        @foreach($errors->all() as $error)
-          <p class="alert alert-danger">{{ $error }}</p>
+
+      <form class="form-horizontal" method="post">
+
+        @foreach ($errors->all() as $error)
+        <p class="alert alert-danger">{{ $error }}</p>
         @endforeach
-        @if(session('status'))
-          <div class="alert alert-success">
-            {{ session('status') }}
-          </div>
+
+        @if (session('status'))
+        <div class="alert alert-success">
+          {{ session('status') }}
+        </div>
         @endif
+
+        {!! csrf_field() !!}
 
         <fieldset>
           <legend>Edit user</legend>
           <div class="form-group">
             <label for="name" class="col-lg-2 control-label">Name</label>
+
             <div class="col-lg-10">
-              <input type="text" class="form-control" id="name" placeholder="Name" name="name" value="{{ $user->name }}">
+              <input type="text" class="form-control" id="name" placeholder="Name" name="name"
+              value="{{ $user->name }}">
             </div>
           </div>
 
           <div class="form-group">
             <label for="email" class="col-lg-2 control-label">Email</label>
+
             <div class="col-lg-10">
-              <input type="email" class="form-control" id="email" placeholder="Email" email="email" value="{{ $user->email }}">
+              <input type="email" class="form-control" id="email" placeholder="Email" name="email"
+              value="{{ $user->email }}">
             </div>
-          </div>      
+          </div>
 
           <div class="form-group">
             <label for="select" class="col-lg-2 control-label">Role</label>
+
             <div class="col-lg-10">
-              <select name="role[]" id="role" class="form-control" multiple>
+              <select class="form-control" id="role" name="role[]" multiple>
                 @foreach($roles as $role)
-                  <option value="{!! $role->name !!}" @if(in_array($role->name, $selectedRoles)) selected="selected" @endif>{!! $role->name !!}
-                  </option>
+                <option value="{!! $role->name !!}"  @if(in_array($role->name, $selectedRoles))
+                  selected="selected" @endif >{!! $role->name !!}
+                </option>
                 @endforeach
               </select>
             </div>
-          </div>  
-          
+          </div>
+
           <div class="form-group">
             <label for="password" class="col-lg-2 control-label">Password</label>
+
             <div class="col-lg-10">
               <input type="password" class="form-control" name="password">
             </div>
-          </div> 
+          </div>
 
           <div class="form-group">
             <label for="password" class="col-lg-2 control-label">Confirm password</label>
