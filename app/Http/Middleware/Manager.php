263f20928018e308e98dfb960e58370c3e4ed2f2
najibu <ns.najibu@gmail.com>
2017-01-16 18:44:22 +0000

    Restrict access to Manager users

--- app/Http/Middleware/Manager.php
+++ app/Http/Middleware/Manager.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class Manager
 {
@@ -15,6 +16,16 @@ class Manager
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if(!Auth::check()) {
+            return redirect('users/login');
+        } else {
+            $user = Auth::user();
+            if($user->hasRole('manager')) {
+                return $next($request);
+            } else {
+                return redirect('/');
+            }
+        }
+        
     }
 }
