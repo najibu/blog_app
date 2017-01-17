b04da61433ef06cc866f9e965df285ee613161f0
najibu <ns.najibu@gmail.com>
2017-01-17 15:51:54 +0000

    Defined many to many relationships

--- app/Category.php
+++ app/Category.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function posts()
+    {
+      return $this->belongsToMany('Post')->withTimestamps();
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $guarded = ['id'];
+    
+    public function categories()
+    {
+      return $this->belongsToMany('Category')->withTimestamps();
+    }
 }
