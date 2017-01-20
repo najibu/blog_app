abcd78fe5b228433f137ca37fbcc25cb47b455cf
najibu <ns.najibu@gmail.com>
2017-01-20 23:08:41 +0000

    updated db

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => parse_url(env("DATABASE_URL"))["host"],
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
